9dbb2263ca98ee251ce96f4762a5ceb18d5093ae
AndreaT <andrea@wazabit.it>
2024-07-06 15:45:39 +0200

    Added Auth0 Authentication

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,46 @@
-from flask import Flask, render_template
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+
+from flask import Flask, render_template, redirect, session, url_for
 from flask_cors import CORS
 import os
 
 from frontend.views import bias, opacity, risk
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+UPLOAD_FOLDER = os.path.abspath("uploads")
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+app.register_blueprint(bias.bp)
+app.register_blueprint(opacity.bp)
+app.register_blueprint(risk.bp)
+
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -19,9 +57,44 @@ app.register_blueprint(risk.bp)
 
 
 @app.route('/', methods=['GET'])
+#def home():
+#    return render_template('homepage.html')
 def home():
-    return render_template('homepage.html')
+    return render_template(
+        "homepage.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4),
+    )
 
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- frontend/views/bias.py
+++ frontend/views/bias.py
@@ -1,3 +1,5 @@
+import json
+
 import os
 import subprocess
 from subprocess import check_output
@@ -30,72 +32,81 @@ if os.system("test -f /.dockerenv") == 0:
     print(f"localhost_ip={localhost_ip}", flush=True)
 
 
-
 @bp.route('/', methods=['GET', 'POST'])
 def home_bias():
-    global used_df
-    global success_status
-    global animation_status
-    global dict_vars  
-    if request.method == 'GET' and request.args.get('reset'):
-        used_df = ""
-        success_status = "text-warning"
-        animation_status = ""
-        dict_vars = {}
-        session.clear()
-    if request.method == 'POST':
-        keys = list(request.files.keys())
-        uploads = [request.files[x].filename for x in keys if x != '']
-        if all(x == '' for x in uploads):
-            flash('No files uploaded, try again!', 'danger')
+    if session.get("user") == None:
+        return redirect(url_for('login'))
+    else:
+        global used_df
+        global success_status
+        global animation_status
+        global dict_vars
+        if request.method == 'GET' and request.args.get('reset'):
+            used_df = ""
+            success_status = "text-warning"
+            animation_status = ""
+            dict_vars = {}
+            session.clear()
+        if request.method == 'POST':
+            keys = list(request.files.keys())
+            uploads = [request.files[x].filename for x in keys if x != '']
+            if all(x == '' for x in uploads):
+                flash('No files uploaded, try again!', 'danger')
+                animation_status = ""
+                return redirect('/bias')
+            if 'dataset' in list(request.files.keys()):
+                dataframe_file = request.files['dataset']
+                dict_vars['dataset'] = dataframe_file.filename
+                if allowed_file(dict_vars['dataset']):
+                    dataframe_file.save(os.path.join(
+                        current_app.config['UPLOAD_FOLDER'], dict_vars['dataset']))
+                    used_df = dict_vars['dataset']
+                    success_status = "text-success"
+                    flash('Dataframe successfully uploaded!', 'success')
+                else:
+                    flash('Unsupported dataframe format.', 'danger')
+                    animation_status = ""
+                    return redirect('/bias')
+            if ('dataset_custom' and 'notebook') in list(request.files.keys()):
+                dict_vars['dataset_custom'] = request.files['dataset_custom'].filename
+                dict_vars['notebook'] = request.files['notebook'].filename
+                used_df = "Custom preprocessed " + dict_vars['dataset_custom']
+                success_status = "text-success"
+                if allowed_file(dict_vars['notebook']) and allowed_file(dict_vars['dataset_custom']):
+                    request.files['dataset_custom'].save(os.path.join(
+                        current_app.config['UPLOAD_FOLDER'], dict_vars['dataset_custom']))
+                    request.files['notebook'].save(os.path.join(
+                        current_app.config['UPLOAD_FOLDER'], dict_vars['notebook']))
+                else:
+                    used_df = ""
+                    dict_vars = {}
+                    session.clear()
+                    success_status = "text-warning"
+                    flash(
+                        'Unsupported notebook or dataset format. Dataset supported types: .pkl, .csv; notebook supported types: .py, .ipynb',
+                        'danger')
+                    animation_status = ""
+                    return redirect('/bias')
+                if request.files['artifacts'].filename != '':
+                    handle_multiupload(request, 'artifacts',
+                                       current_app.config['UPLOAD_FOLDER'])
+                notebook_extension = dict_vars['notebook'].split('.')[1]
+                match notebook_extension:
+                    case 'ipynb':
+                        os.system("jupyter nbconvert --to python " +
+                                  os.path.join(current_app.config['UPLOAD_FOLDER'], dict_vars['notebook']))
+                        note_name = dict_vars['notebook'].split('.')[0]
+                        subprocess.run(["python3", os.path.join(
+                            current_app.config['UPLOAD_FOLDER'], note_name + ".py")])
+                    case 'py':
+                        subprocess.run(["python3", os.path.join(
+                            current_app.config['UPLOAD_FOLDER'], dict_vars['notebook'])])
+                flash('Custom preprocessing pipeline successfully uploaded and processed!', 'success')
             animation_status = ""
             return redirect('/bias')
-        if 'dataset' in list(request.files.keys()):
-            dataframe_file = request.files['dataset']
-            dict_vars['dataset'] = dataframe_file.filename
-            if allowed_file(dict_vars['dataset']):
-                dataframe_file.save(os.path.join(
-                    current_app.config['UPLOAD_FOLDER'], dict_vars['dataset']))
-                used_df = dict_vars['dataset']
-                success_status = "text-success"
-                flash('Dataframe successfully uploaded!', 'success')
-            else:
-                flash('Unsupported dataframe format.', 'danger')
-                animation_status = ""
-                return redirect('/bias')
-        if ('dataset_custom' and 'notebook') in list(request.files.keys()):
-            dict_vars['dataset_custom'] = request.files['dataset_custom'].filename
-            dict_vars['notebook'] = request.files['notebook'].filename
-            used_df = "Custom preprocessed " + dict_vars['dataset_custom']
-            success_status = "text-success"
-            if allowed_file(dict_vars['notebook']) and allowed_file(dict_vars['dataset_custom']):
-                request.files['dataset_custom'].save(os.path.join(
-                    current_app.config['UPLOAD_FOLDER'], dict_vars['dataset_custom']))
-                request.files['notebook'].save(os.path.join(
-                    current_app.config['UPLOAD_FOLDER'], dict_vars['notebook']))
-            else:
-                used_df = ""
-                dict_vars = {}
-                session.clear()
-                success_status = "text-warning"
-                flash('Unsupported notebook or dataset format. Dataset supported types: .pkl, .csv; notebook supported types: .py, .ipynb', 'danger')
-                animation_status = ""
-                return redirect('/bias')
-            if request.files['artifacts'].filename != '':
-                handle_multiupload(request, 'artifacts',
-                                   current_app.config['UPLOAD_FOLDER'])
-            notebook_extension = dict_vars['notebook'].split('.')[1]
-            match notebook_extension:
-                case 'ipynb':
-                    os.system("jupyter nbconvert --to python " +
-                              os.path.join(current_app.config['UPLOAD_FOLDER'], dict_vars['notebook']))
-                    note_name = dict_vars['notebook'].split('.')[0]
-                    subprocess.run(["python3", os.path.join(
-                        current_app.config['UPLOAD_FOLDER'], note_name + ".py")])
-                case 'py':
-                    subprocess.run(["python3", os.path.join(
-                        current_app.config['UPLOAD_FOLDER'], dict_vars['notebook'])])
-            flash('Custom preprocessing pipeline successfully uploaded and processed!', 'success')
-        animation_status = ""
-        return redirect('/bias')
-    return render_template('home.html', df_used=used_df, status=success_status, animated=animation_status)
+        return render_template('home.html',
+                               session=session.get("user"),
+                               pretty=json.dumps(session.get("user"), indent=4),
+                               df_used=used_df,
+                               status=success_status,
+                               animated=animation_status)
