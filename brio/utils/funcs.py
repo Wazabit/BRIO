4bf6271e621fa92d6f99b5c73c1633f571d6b18f
AndreaT <andrea@wazabit.it>
2024-07-13 13:24:07 +0200

    Fixed default value for REFERENCE DISTRIBUTIONS inputs

--- brio/utils/funcs.py
+++ brio/utils/funcs.py
@@ -68,20 +68,33 @@ def write_reference_distributions_html(rootvar: str, targetvar: str, df: pd.Data
         nrows += 1
     c = 0
     d = 0
+    current_value = 1
+    current_target = ntarget
     for n in range(nrows):
         tot_html += f'<div class="row" id="ref_dist{n}">'
         for j in range(tot_refs):
             if j != 0 and j % 4 == 0:
                 break
             tot_refs -= 1
+
+            if current_target == 0:
+                current_value = 1
+                current_target = ntarget
+
+            value = round((current_value / current_target), 2)
             tot_html += '<div class="col-3 d-flex flex-column align-items-center">'
             tot_html += f'<label for="prob_{c}_{d}">{rootvar}_{c}_{targetvar}_{d}_ref</label>'
-            tot_html += f'<input type="number" class="form-control number-mirai w-100 mb-3" placeholder="{rootvar}_{c}_{targetvar}_{d}_ref" name="prob_{c}_{d}" id="prob_{c}_{d}" min="0" max="1" step=".01">'
+            tot_html += f'<input type="number" data-tot={ntarget} class="form-control number-mirai w-100 mb-3 ref-input" value={value} name="prob_{c}_{d}" id="prob_{c}_{d}" min="0" max="1" step=".01">'
             d += 1
             if d == ntarget:
                 d = 0
                 c += 1
+
+            current_value = current_value - value
+            current_target -= 1
+
             tot_html += '</div>'
+
         tot_html += '</div>'
     return tot_html
 
